8827ce809ee9de8bf3a8c3a493d0357570e3b608
lauCodePink <laura.maldonado@est.iudigital.edu.co>
2025-12-02 23:46:51 -0500

    usuario model con db

--- v1/src/main/java/com/rollerspeed/v1/Model/UsuarioModel.java
+++ v1/src/main/java/com/rollerspeed/v1/Model/UsuarioModel.java
@@ -1,6 +1,31 @@
 package com.rollerspeed.v1.Model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Table(name = "tbl_usuarios")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UsuarioModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(name = "nombre", nullable = true, length = 50)
+    String name;
+
+    
+    @Column(name = "password", nullable = false, length = 100) 
+    String password;
 
     
 }
